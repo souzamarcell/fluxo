b95a8db61f8618c7a2ef4526661ba691eb3a4d7d
Marcell Souza <marcell.ramos@gmail.com>
2025-08-07 09:45:22 -0400

    party 1.0.6 Erros corrigidos

--- src/app/pages/entradas/aplicacoes/page.tsx
+++ src/app/pages/entradas/aplicacoes/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { AuthGuard } from '@/app/components/AuthGuard';
+import { AuthGuard } from '@/components/AuthGuard';
 
 export default function AplicacoesPage() {
   const router = useRouter();
